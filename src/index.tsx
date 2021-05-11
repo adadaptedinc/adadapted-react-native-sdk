b411feb68a3c8aa8c9a189104b091e3fd219693b
mkruk2 <mkruk@adadapted.com>
2021-05-11 15:02:25 -0400

    fix: update npm

--- src/index.tsx
+++ src/index.tsx
@@ -727,7 +727,9 @@ export class AdadaptedReactNativeSdk {
                             this.apiEnv
                         )
                         .then((response) => {
-                            NativeModules.AdadaptedReactNativeSdk.storeCurrentSessionId(response.data.session_id);
+                            NativeModules.AdadaptedReactNativeSdk.storeCurrentSessionId(
+                                response.data.session_id
+                            );
                             this.sessionId = response.data.session_id;
                             this.sessionInfo = response.data;
                             this.adZones = this.generateAdZones(
