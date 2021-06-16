b2c7320b63d93affce3e32e603aceca8a4823e30
mkruk2 <mkruk@adadapted.com>
2021-06-16 13:13:27 -0400

    fix: iOS 14 app tracking

--- ios/AdadaptedReactNativeSdk.h
+++ ios/AdadaptedReactNativeSdk.h
@@ -1,5 +1,6 @@
 #import <React/RCTBridgeModule.h>
 #import <AdSupport/ASIdentifierManager.h>
+#import <AppTrackingTransparency/ATTrackingManager.h>
 
 @interface AdadaptedReactNativeSdk : NSObject <RCTBridgeModule>
 
